1b119c7cb89e9ca790cc1bf83ebd4e8975a632ab
cmtoomey <chris.toomey@slalom.com>
2015-10-27 17:48:33 -0700

    added markers, popups, centering

--- code.js
+++ code.js
@@ -1,13 +1,53 @@
 $(document).ready(function() {
-
+  var color = "#123456";
   $("#globe").click(function() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRvaDkxIiwiYSI6IjQxN2IyMzFkNzgwODEwZjdiZGEzNzEzMTE1MjQyMzc5In0.NJQwPueh-IOZ7V14d8NILA';
     var map = new mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-      center: [-74.50, 40], // starting position
-      zoom: 9 // starting zoom
+      center: [-77.035346, 38.907757], // starting position
+      zoom: 12 // starting zoom
     });
-    map.addControl(new mapboxgl.Navigation());
-  })
+    var url = 'http://api.tiles.mapbox.com/v3/examples.map-zr0njcqy/markers.geojson';
+    var source = new mapboxgl.GeoJSONSource({
+      data: url
+    });
+
+    map.on('style.load', function() {
+      map.addSource('drone', source);
+      map.addLayer({
+        "id": "drone",
+        "interactive": "true",
+        "type": "symbol",
+        "source": "drone",
+        "type": "circle",
+        // "layout": {
+        //   "icon-image": "marker-15",
+        // },
+        "paint": {
+          "circle-color": color,
+          "circle-radius": "25",
+        }
+      });
+    });
+
+    map.on('click', function(e) {
+      map.featuresAt(e.point, {
+        layer: 'drone',
+        radius: 10,
+        includeGeometry: true
+      }, function(err, features) {
+        if (err) throw err;
+        if (features.length) {
+          map.flyTo({
+            center: features[0].geometry.coordinates
+          });
+          var tooltip = new mapboxgl.Popup()
+          .setHTML('<h1>'+features[0].properties.title+'</h1>'+'\n'+'<h2>'+features[0].properties.description+'</h2>')
+          .addTo(map);
+        }
+      });
+    })
+
+  });
 })
